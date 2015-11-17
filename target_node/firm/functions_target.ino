237a642197d6aace8b50c5698150b44337f8ea4a
n0p <lbernal@gmail.com>
2015-11-17 20:27:03 +0100

    Added previous files & staging schematics

---
+++ target_node/firm/functions_target.ino
@@ -0,0 +1,74 @@
+void lc_to_string(led_command* lc_tmp){
+  Serial.print("| LED ID: ");
+  Serial.print(lc_tmp->led_id);
+  Serial.print("| Red: ");
+  Serial.print(lc_tmp->R);
+  Serial.print("| Green: ");
+  Serial.print(lc_tmp->G);
+  Serial.print("| Blue: ");
+  Serial.print(lc_tmp->B);
+  Serial.print("| Pattern: ");
+  Serial.println(lc_tmp->pattern);
+}
+
+#ifdef TARGET_NODE
+
+/**************************************
+* Sets a received led command 
+**************************************/
+boolean set_led_command(led_command* lc_tmp){
+  if (lc_tmp->pattern > 3) {
+    Serial.println("Pattern out of bounds. Corrupted data?");
+    return false;
+  }
+  //If led_id from incoming led command is 0, result is applied to
+  //every LED on the board
+  Serial.println("Setting new configuration");
+  if(lc_tmp->led_id == 0){
+    for(int i=0; i<NUM_LEDS; i++){
+      memcpy(&lc[i],&lc_tmp,sizeof(lc_tmp));
+    }
+  } else {
+    memcpy(&lc[lc_tmp->led_id - 1],&lc_tmp,sizeof(lc_tmp));
+  }
+  Serial.println("New configuration:");
+  //Command received to string:
+  lc_to_string(lc_tmp);
+  free(lc_tmp);
+  return 1;
+}
+/**************************************
+* Quick function to turn all leds off 
+**************************************/
+boolean turn_off_all(){
+  //TO_DO
+  return 0;
+}
+/**************************************
+* Post requested led_command to master
+* NOTE: if led_id = 0, post led count
+*************************************/
+boolean post_led_command(uint8_t led_id){
+  //TO_DO
+  return 0;
+}
+/**************************************
+* Post node version to master 
+**************************************/
+boolean post_version(){
+  //TO_DO
+  return 0;
+}
+
+void update_leds(){
+  frame = frame % 24;
+  for(int i=0; i<NUM_LEDS; i++){ //for each led
+    uint8_t shift_frame = mask_frame[lc[i].pattern][frame];
+    analogWrite(led_pinout[i][0],lc[i].R >> shift_frame);
+    analogWrite(led_pinout[i][1],lc[i].G >> shift_frame);
+    analogWrite(led_pinout[i][2],lc[i].B >> shift_frame);
+  }
+  //Serial.print("Frame ");
+  //Serial.println(frame++);  
+}
+#endif
---
+++ target_node/firm/lib_target.h
@@ -0,0 +1,28 @@
+typedef struct {
+  uint8_t led_id;
+  uint8_t pattern;
+  uint8_t R;
+  uint8_t G;
+  uint8_t B;
+} led_command;
+
+unsigned long display_timer=0;
+//Target node prototypes and declarations
+#ifdef TARGET_NODE
+
+#define NODE_ID 2
+#define NUM_LEDS 1
+boolean int_flag = 0;
+boolean int_proc = 0;
+uint8_t frame = 0;
+const int led_pinout[NUM_LEDS][3] = {{3,5,6}};
+led_command lc[NUM_LEDS];
+
+const uint8_t mask_frame[4][24] = {
+                                   {8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8},
+                                   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                                   {0,0,0,0,0,0,0,0,0,0,0,0,8,8,8,8,8,8,8,8,8,8,8,8},
+                                   {0,0,0,0,0,0,8,8,8,8,8,8,0,0,0,0,0,0,8,8,8,8,8,8}
+                                  };
+
+#endif
